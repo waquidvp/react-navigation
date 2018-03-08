6ee5e903a3c189663cc9bbb16a6ec157f3221b01
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2018-03-08 19:32:27 +0000

    Tab bar center

--- src/views/TabView/TabBarTop.js
+++ src/views/TabView/TabBarTop.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, StyleSheet } from 'react-native';
+import { Animated, StyleSheet, View } from 'react-native';
 import { TabBar } from 'react-native-tab-view';
 import TabBarIcon from './TabBarIcon';
 
@@ -110,13 +110,15 @@ export default class TabBarTop extends React.PureComponent {
     const props = this.props;
 
     return (
-      <TabBar
-        {...props}
-        onTabPress={this._handleOnPress}
-        jumpToIndex={() => {}}
-        renderIcon={this._renderIcon}
-        renderLabel={this._renderLabel}
-      />
+      <View style={styles.tabBarContainer} >
+        <TabBar
+          {...props}
+          onTabPress={this._handleOnPress}
+          jumpToIndex={() => {}}
+          renderIcon={this._renderIcon}
+          renderLabel={this._renderLabel}
+        />
+      </View>
     );
   }
 }
@@ -131,5 +133,8 @@ const styles = StyleSheet.create({
     fontSize: 13,
     margin: 8,
     backgroundColor: 'transparent',
+  },
+  tabBarContainer: {
+    alignItems: 'center',
   },
 });
